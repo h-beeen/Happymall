d845935f2420d7da7635d2a632e25ff49559d661
Haebin <hbyeon@student.42seoul.kr>
2023-03-25 21:20:39 +0900

    🔧 [Week 3] fix: JUnit5 TDD Compatibility Error

--- src/test/java/JpashopApplicationTests.java
+++ src/test/java/JpashopApplicationTests.java
@@ -1,7 +1,11 @@
+import jpabook.jpashop.JpashopApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = JpashopApplication.class)
 class JpashopApplicationTests {
 
 	@Test
--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -5,17 +5,14 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 @Transactional
 class MemberServiceTest {
 
